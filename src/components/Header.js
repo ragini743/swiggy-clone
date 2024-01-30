aa6708516f04c4de0f445b1f9d76df634dff7267
Ragini <raginiv743@gmail.com>
2024-01-30 23:28:08 +0530

    start creating swiggy-clone

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+        <div>
+            <img src=""alt=""></img>
+        </div>
+        <p>Location : allahabad</p>
+        </div>
+        <div>
+            <div>
+                <input type='text'></input>
+            </div>
+            <ul className="">
+                <li>
+                    <div>
+                        <img src=""alt=""></img>
+                    </div>
+                    offer
+                </li>
+                <li>
+                    <div>
+                      <img src='' alt =""></img>
+                    </div>
+                    help
+                </li>
+                <li>
+                    <div>
+                        <img src=''alt=''></img>
+                    </div>
+                    signIn
+                    
+                </li>
+                <li>
+                    <div>
+                        <img src=''alt =""></img>
+                    </div>
+                    cart
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
