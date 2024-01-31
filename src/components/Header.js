9bc9f4388b18e51ae5e0233c7a73f802191a71ee
Ragini <raginiv743@gmail.com>
2024-01-31 15:04:08 +0530

    add hover

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const arrayOfContent = [
     id: "4",
   },
   {
-    src: "https://www.shutterstock.com/image-vector/shopping-cart-vector-icon-on-260nw-1149438545.jpg",
+    src: "https://static-00.iconduck.com/assets.00/shopping-cart-icon-251x256-7876j5yc.png",
     text: "Cart",
     id: "5",
   },
@@ -30,21 +30,21 @@ const arrayOfContent = [
 export const Content = ({ content }) => {
   return (
     <div className="flex items-center mr-2 justify-center flex-grow basis-0 ">
-      <div className="w-[20%] md:w-[10%] mr-1">
+      <div className="w-[20%] md:w-[15%] mr-1 md:mr-2 ">
         <img
           src={content.src}
           alt=""
-          className="w-[100%] box-border object-cover"
+          className="w-[100%] object-cover h-auto "
         ></img>
       </div>
-      <div className=" text-[8px] md:text-sm">{content.text}</div>
+      <div className=" text-[8px] md:text-lg text-gray-800 hover:text-orange-500">{content.text}</div>
     </div>
   );
 };
 
 const Header = () => {
   return (
-    <div className=" flex justify-evenly w-[100%] shadow-lg bg-white z-30 fixed py-2 md:px-20 md:py-4 font-bold">
+    <div className=" flex justify-evenly w-[100%] shadow-lg bg-white z-30 fixed py-2 md:px-20 md:py-4">
       <div className="flex w-[50%] justify-between items-center">
         <div className="flex items-center">
           <div className="w-[15%] md:w-[20%]">
