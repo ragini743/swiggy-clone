9886216ef7e550967ed24311f12a546d3bb26c54
Ragini <raginiv743@gmail.com>
2024-02-04 16:03:39 +0530

    completed the UI

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -9,22 +9,22 @@ const Restaurants = ({ restaurantData }) => {
   return (
     <div>
       <div className="h-full text-[.5rem] sm:text-sm grid gap-5">
-        <div className="rounded-2xl ">
+        <div className="rounded-xl w-[100%] h-[100%] ">
           <img
             src={url}
             alt=""
-            className="h-full w-full object-cover object-center"
+            className=" w-96 h-44 object-cover object-center rounded-xl"
           ></img>
         </div>
-        <div>
-        <p>{name} </p>
-        <div>
+        <div className="md:text-[1rem]">
+        <p className="font-bold md:text-lg">{name} </p>
+        <div className="font-bold md:text-[1rem] pt-1">
           <span className="bg-green-600 text-white font-extrabold px-2">*</span>
           <span>{avgRating}</span>
           <span>. {sla.slaString}</span>
         </div>
-        <div>{cuisines.join(",")} </div>
-        <p>{areaName}</p>
+        <div className="pt-1">{cuisines.join(",")} </div>
+        <p className="pt-1">{areaName}</p>
         </div>
       
       </div>
