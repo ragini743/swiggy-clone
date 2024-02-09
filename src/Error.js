97fdcb348346834be538e6c63746f6d1275b1c52
Ragini <raginiv743@gmail.com>
2024-02-09 13:36:37 +0530

    completed the error page style

--- src/Error.js
+++ src/Error.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
 const Error = () => {
-    const error = useRouteError()
-    console.log("error",error)
+    const errors = useRouteError()
+    console.log("errors",errors)
   return (
-    <div>
-        <h1>Ooops!</h1>
-        <p>something went wrong</p>
+    <div className='bg-cyan-50 h-[100vh] font-bold text-lg pl-10 pt-10'>
+        <h1 className='text-2xl '>Ooops!</h1>
+        <p className='text-red-500'>something went wrong</p>
+        <p>{errors.data}</p>
+        <p>{errors.error.message}</p>
+        <p>{errors.error.stack}</p>
     </div>
   )
 }
