32ce9931d191fa3454e478afeac585ce7af9ebb7
Ragini <raginiv743@gmail.com>
2024-02-12 11:07:39 +0530

    create signIn and LogIn form

--- src/form/SignIn.js
+++ src/form/SignIn.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { checkValidData } from './validate'
 
 const SignIn = () => {
   const [isSignInForm,setIsSignInForm] = useState(false)
@@ -7,11 +8,11 @@ const SignIn = () => {
   }
 
   return (
-    <div className='w-[90%] max-w-[40%] mx-auto mt-10'>
+    <div className='w-[90%] sm:w-[80%] md:w-[50%] lg:w-[40%] mx-auto mt-10'>
       <div className='flex flex-col justify-center p-4 '>
          <div className='text-lg'>
             <h1 className='text-[2rem] font-bold'>{isSignInForm?"Sign up":"Login"}</h1>
-            <h1>or <span className='text-orange-500' onClick={toggleButton}>{isSignInForm?"or login to your account":"create an account"}</span></h1>
+            <h1 className='mt-2'>or <span className='text-orange-500' onClick={toggleButton}>{isSignInForm?"or login to your account":"create an account"}</span></h1>
             <div>
                 <img src="" alt=''>
 
@@ -19,17 +20,17 @@ const SignIn = () => {
             </div>
         </div>
         <div className=' flex flex-col mt-5 '>
-          {isSignInForm?null:(<div className='flex flex-col border-2 border-gray-400 pl-4 py-4'>
+          <div className='flex flex-col border-2 border-gray-400 pl-4 py-4'>
             <label for="number">Phone number</label>
             <input type ="number"id="number" className='outline-none'min="0" max="0"></input>
-          </div>)}
+          </div>
           
-          {isSignInForm?(<div className='flex flex-col border-b-2 border-gray-400'>
+          {isSignInForm?(<div className='flex flex-col border-b-2 border-l-2 border-r-2 border-gray-400 pl-4 py-4'>
             <label for="name">Name</label>
             <input type ="text" id="name" className='outline-none'></input>
           </div>):null}
           
-        {isSignInForm?(<div className='flex flex-col'>
+        {isSignInForm?(<div className='flex flex-col border-b-2 border-l-2 border-r-2 border-gray-400 pl-4 py-4'>
             <label for="email">Email</label>
             <input type ="email" id="email" className='outline-none'></input>
           </div>):null}
@@ -37,12 +38,12 @@ const SignIn = () => {
        
           
         </div>
-        <p className='text-blue-500'> have a referral code</p>
-        <div className=' text-white'>
-       <button type='submit'>continue</button>
+        <p className='text-blue-600 font-bold'>{isSignInForm?" Have a referral code ? ":null}</p>
+        <div className=' text-white bg-orange-500 mt-5 text-center'>
+       <button type='submit'className='pl-4 py-4 '>{isSignInForm?"Continue":"Login"}</button>
         </div>
       
-        <p className='text-slate-400'>By creating an account, I accept the<span> Terms & Conditions & Privacy Policy
+        <p className='text-slate-600 mt-2'>{isSignInForm?"By creating an account":"By clicking on Login"} , I accept the<span className='text-black'> Terms & Conditions & Privacy Policy
         </span>
         </p>
       </div>
