0b7ae332655019f5a851714c3efab0643d80161c
Ragini <raginiv743@gmail.com>
2024-02-12 11:39:38 +0530

    add form to preventDefault

--- src/form/SignIn.js
+++ src/form/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { checkValidData } from './validate'
 
 const SignIn = () => {
@@ -6,11 +6,18 @@ const SignIn = () => {
   const toggleButton =() =>{
    setIsSignInForm(!isSignInForm)
   }
+  
+const email =useRef(null)
+const number =useRef(null)
+const handleValidation =(email) =>{
+  console.log("email",email)
+  // checkValidData(email,password)
+}
 
   return (
     <div className='w-[90%] sm:w-[80%] md:w-[50%] lg:w-[40%] mx-auto mt-10'>
-      <div className='flex flex-col justify-center p-4 '>
-         <div className='text-lg'>
+      <form className='flex flex-col justify-center p-4 ' onSubmit={(e)=>e.preventDefault} >
+         <div className='text-lg cursor-pointer'>
             <h1 className='text-[2rem] font-bold'>{isSignInForm?"Sign up":"Login"}</h1>
             <h1 className='mt-2'>or <span className='text-orange-500' onClick={toggleButton}>{isSignInForm?"or login to your account":"create an account"}</span></h1>
             <div>
@@ -21,32 +28,32 @@ const SignIn = () => {
         </div>
         <div className=' flex flex-col mt-5 '>
           <div className='flex flex-col border-2 border-gray-400 pl-4 py-4'>
-            <label for="number">Phone number</label>
-            <input type ="number"id="number" className='outline-none'min="0" max="0"></input>
+            <label htmlFor="number">Phone number</label>
+            <input type ="number"id="number"ref={number} className='outline-none'min="0" max="0"></input>
           </div>
           
           {isSignInForm?(<div className='flex flex-col border-b-2 border-l-2 border-r-2 border-gray-400 pl-4 py-4'>
-            <label for="name">Name</label>
+            <label htmlFor="name">Name</label>
             <input type ="text" id="name" className='outline-none'></input>
           </div>):null}
           
         {isSignInForm?(<div className='flex flex-col border-b-2 border-l-2 border-r-2 border-gray-400 pl-4 py-4'>
-            <label for="email">Email</label>
-            <input type ="email" id="email" className='outline-none'></input>
+            <label htmlFor="email">Email</label>
+            <input type ="email"ref={email} id="email" className='outline-none'></input>
           </div>):null}
           
        
           
         </div>
         <p className='text-blue-600 font-bold'>{isSignInForm?" Have a referral code ? ":null}</p>
-        <div className=' text-white bg-orange-500 mt-5 text-center'>
-       <button type='submit'className='pl-4 py-4 '>{isSignInForm?"Continue":"Login"}</button>
+        <div className=' text-white bg-orange-500 mt-5 text-center cursor-pointer'>
+       <button type='submit'className='pl-4 py-4  '>{isSignInForm?"Continue":"Login"}</button>
         </div>
       
-        <p className='text-slate-600 mt-2'>{isSignInForm?"By creating an account":"By clicking on Login"} , I accept the<span className='text-black'> Terms & Conditions & Privacy Policy
+        <p className='text-slate-600 mt-2 cursor-pointer'>{isSignInForm?"By creating an account":"By clicking on Login"} , I accept the<span className='text-black'> Terms & Conditions & Privacy Policy
         </span>
         </p>
-      </div>
+      </form>
        
     </div>
     
--- src/form/validate.js
+++ src/form/validate.js
@@ -1,8 +1,8 @@
-export const checkValidData = (email,password) =>{
+export const checkValidData = (email) =>{
     const isEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.text(email);
-    const isPasswordValid =/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/.text(password);
+    // const isPasswordValid =/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/.text(password);
 
     if(!isEmailValid) return "email id is not valid";
-    if(!isPasswordValid) return"password is not valid";
-    return null;
+    // if(!isPasswordValid) return"password is not valid";
+    // return null;
 }
