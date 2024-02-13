be3e96806e649b7f35e8d4f86decfee7f70687b6
Ragini <raginiv743@gmail.com>
2024-02-13 16:47:55 +0530

    create state variable and make api call

--- src/help/HelpSUpport.js
+++ src/help/HelpSUpport.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PartenerBoarding from './PartenerBoarding'
 import { Link } from 'react-router-dom'
 
 const HelpSUpport = () => {
+  const [listTitle,setListTitle] = useState([])
+  const titleFetchData = async() =>{
+    const data = await fetch("https://www.swiggy.com/dapi/support?")
+  }
+  useEffect(()=>{
+    titleFetchData()
+  },[])
   return (
     <div className='pt-16 md:pt-28 bg-blue-500 text-white px-10 h-[100vh]'>
         <div className='mb-2'>
@@ -15,10 +22,11 @@ const HelpSUpport = () => {
 
         <div className='bg-white mt-8 '>
           <aside className='bg-sky-100 text-gray-600 px-10 pt-5 pb-10 '>
-            <h1 className='hover:bg-white'><Link to="issue/partener-onboarding">
+            {/* <h1 className='hover:bg-white hover:text-yellow-600'><Link to="issue/partener-onboarding">
               Partener Boarding</Link></h1>
-            <h1 className='hover:bg-white'>Legal</h1>
-            <h1 className='hover:bg-white'>FAQs</h1>
+            <h1 className='hover:bg-white hover:text-yellow-600'>Legal</h1>
+            <h1 className='hover:bg-white hover:text-yellow-600'>FAQs</h1> */}
+
           </aside>
         </div>
     </div>
--- src/help/PartenerBoarding.js
+++ src/help/PartenerBoarding.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const PartenerBoarding = () => {
   return (
     <div></div>
