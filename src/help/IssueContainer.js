39696a93208170543f5e4c9a5d6802e6f55ca670
Ragini <raginiv743@gmail.com>
2024-02-15 13:45:23 +0530

    save images and create state

--- src/help/IssueContainer.js
+++ src/help/IssueContainer.js
@@ -7,66 +7,87 @@ import {
 } from "../utils/issueData";
 
 export const IssueList = ({ data }) => {
-
-  const { title, hyperLink, description, hyperLinkText } = data ;
-  console.log(data.options[0])
-let descriptionList
-  if(description) ( descriptionList = description.split("\n").map((item, index) => (
-    <li key={index} className="list-none">{item}</li>
-  )))
+  const [activeId, setActiveId] = useState(false);
+  const { title, hyperLink, description, hyperLinkText } = data;
+  console.log(data);
+  let descriptionList;
+  if (description)
+    descriptionList = description.split("\n").map((item, index) => (
+      <li key={index} className="list-none">
+        {item}
+      </li>
+    ));
 
   return (
     <div className=" py-4  mx-auto overflow-scroll pb-5">
       <div className="pl-4">
-        <h1 className="text-xs sm:text-lg text-black">{title}</h1>
-        <h2 className="text-[0.5rem] sm:text-sm text-black pb-4">
-          {description ? descriptionList : null}
-        </h2>
-        <h2 className="text-orange-400 font-semibold text-[0.5rem] sm:text-sm pb-4">
-          <a href={hyperLink ? hyperLink : null} rel="noreferrer" target="_blank">{hyperLinkText}</a>
-        </h2>
+        <div>
+          <h1 className="text-xs sm:text-lg text-black">{title}</h1>
+          <div></div>
+        </div>
 
-        {data.options.emailId !== undefined ? (
-          <div>
-            <a href={data.options.emailId}>send a mail</a>
-          </div>
-        ) : null}
-        {data.options.length!==0?( <div className="">
-          <div className="border-2 border-orange-400 text-orange-400 font-bold inline p-2">  <a href=" mailto:partnersupport@swiggy.in?subject=I%20want%20to%20partner%20my%20restaurant%20with%20Swiggy"rel="noreferrer" target="_blank">Send an email</a>
-          </div>
-          <h3 className="text-black text-[0.5rem] sm:text-xs mt-3">
-            {data.options[0].waitTime}</h3>
-        </div>):null}
-      
+        <div>
+          <h2 className="text-[0.5rem] sm:text-sm text-black pb-4">
+            {description ? descriptionList : null}
+          </h2>
+          <h2 className="text-orange-400 font-semibold text-[0.5rem] sm:text-sm pb-4">
+            <a
+              href={hyperLink ? hyperLink : null}
+              rel="noreferrer"
+              target="_blank"
+            >
+              {hyperLinkText}
+            </a>
+          </h2>
+
+          {data.options.emailId !== undefined ? (
+            <div>
+              <a href={data.options.emailId}>send a mail</a>
+            </div>
+          ) : null}
+          {data.options.length !== 0 ? (
+            <div className="">
+              <div className="border-2 border-orange-400 text-orange-400 font-bold inline p-2">
+                {" "}
+                <a
+                  href=" mailto:partnersupport@swiggy.in?subject=I%20want%20to%20partner%20my%20restaurant%20with%20Swiggy"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Send an email
+                </a>
+              </div>
+              <h3 className="text-black text-[0.5rem] sm:text-xs my-3">
+                {data.options[0].waitTime}
+              </h3>
+            </div>
+          ) : null}
+        </div>
       </div>
       <hr className="shadow-md "></hr>
     </div>
   );
 };
 
-const IssueContainer = ({issueType ,setIssueType}) => {
+const IssueContainer = ({ issueType, setIssueType }) => {
   // console.log('issuecontainertype', issueType);
   const [issueData, setIssueData] = useState([]);
 
   const partnerFetchData = async () => {
     let resolvedData;
-   
+
     if (issueType.type === "partner-onboarding")
       resolvedData = partnerIssuedata;
-    else if (issueType.type === "legal")
-      resolvedData = legelIssuedata;
-    else if (issueType.type === "faq")
-      resolvedData = faqIssuedata;
-  
+    else if (issueType.type === "legal") resolvedData = legelIssuedata;
+    else if (issueType.type === "faq") resolvedData = faqIssuedata;
+
     // console.log("resolvedData:", resolvedData);
-   
+
     if (resolvedData) {
       setIssueData(resolvedData.data.issues.data);
     } else {
-      setIssueData(partnerIssuedata.data.issues.data
-        ) 
+      setIssueData(partnerIssuedata.data.issues.data);
     }
-  
   };
 
   useEffect(() => {
@@ -80,12 +101,10 @@ const IssueContainer = ({issueType ,setIssueType}) => {
   return (
     <div className="pt-4 issuContianer">
       <h1 className="text-black font-bold text-xl mb-4">{issueType.title}</h1>
-      {issueData.map((data) => {
+      {issueData.map((data, index) => {
         return <IssueList key={data.id} data={data} />;
       })}
-   
     </div>
-
   );
 };
 
